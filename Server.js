e14ba3b94e5de6df9e8be5de86c737c11c31480b
Chibuzor <chibuzorm26@example.com>
2022-09-05 15:48:10 +0100

    fix

--- Server.js
+++ Server.js
@@ -30,5 +30,5 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const port = process.env.PORT || '5000';
+app.set('port', port);
