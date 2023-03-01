964f593351b8338e92c8a9d36519075810609ad3
Chibuzor <chibuzorm26@example.com>
2023-03-01 13:34:40 +0100

    cors

--- Server.js
+++ Server.js
@@ -17,6 +17,14 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/seed", seedRouter);
